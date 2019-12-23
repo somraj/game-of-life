fd8f16540f6a99595451505b6d54767521c4ff8f
somraj <somraj@live.com>
2019-12-23 07:55:50 +0530

    Commit Testing Code again 2
    Commit Testing Code again 2

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
  *
  * HomePageController.java
  * Controller to navigate to the main page
- * Testing Code again
+ * Testing Code again 2
  */
 @Controller
 public class HomePageController {
